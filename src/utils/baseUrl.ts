504df5332ab95aba690dd9abe13b622d9709798a
jjy <jjyusa2010@gmail.com>
2024-03-21 02:27:42 -0500

    deleted @types/react-ative and is now working on dev build p1

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -7,7 +7,7 @@ const baseUrl = 'http://192.168.1.249:8080/api/v1';
 
 // const baseUrl = `http://${ipAddress}:8080/api/v1`;
 
-// const backendApi = 'https://www.priders.net/api/v1/';
+// const baseUrl = 'https://www.priders.net/api/v1';
 
 export default baseUrl;
 
